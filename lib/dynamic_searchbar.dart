2a298e3c0c9d6708364f5c9aa97df82a909d64dd
bataa07 <baterdene.amarsaikhan@gmail.com>
2023-07-03 03:51:05 +0800

    Update gitignore

--- lib/dynamic_searchbar.dart
+++
@@ -1,7 +0,0 @@
-library dynamic_searchbar;
-
-export 'src/constants/enums.dart';
-export 'src/models/search_action.dart';
-export 'src/models/search_theme_data.dart';
-export 'src/widgets/global_searchbar.dart';
-export 'src/widgets/search_field.dart';
--- lib/src/models/tag.dart
+++
@@ -1,9 +0,0 @@
-class Tag<T> {
-  final bool isSelected;
-  final T entry;
-
-  Tag({
-    this.isSelected = false,
-    required this.entry,
-  });
-}
